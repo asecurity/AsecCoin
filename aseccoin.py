6e8d200b0366120860759866e789c073e28efaaa
asecurity <mike.olden@gmail.com>
2017-12-12 21:05:16 -0600

    Updated files

--- aseccoin.py
+++ aseccoin.py
@@ -1,13 +1,20 @@
 from urllib2 import Request, urlopen, URLError
 import json
+#debug = {'verbose': sys.stderr}
+
+#headers = {'User-Agent': 'AsecCoin 0.1'}
+
 
 #print "This is a test"
 
 request = Request('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-ltc')
 
+
 try:
     response = urlopen(request)
     html = response.read()
     print html
+
+    print "Attempting to print JSON formatted...\n"
 except URLError, e:
     print 'Error processing:', e
