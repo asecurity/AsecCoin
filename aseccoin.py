0b995f02c1fbfe4daa9f21a2aa2a5b4ab0ddd00d
asecurity <mike.olden@gmail.com>
2017-12-13 16:57:04 -0600

    Cleaning up unused methods

--- aseccoin.py
+++ aseccoin.py
@@ -1,14 +1,8 @@
 from urllib2 import Request, urlopen, URLError
 import json
-#debug = {'verbose': sys.stderr}
-
 #headers = {'User-Agent': 'AsecCoin 0.1'}
 
-
-#print "This is a test"
-
 request = Request('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-ltc')
-
 
 try:
     response = urlopen(request)
@@ -21,7 +15,7 @@ try:
         result = loadjson['result']
 
         result1 = json.dumps(result)
-        print(result1)
+        print result1
     else: 
         print "Parsing failed..."
 
