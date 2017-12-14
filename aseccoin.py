c5854721501ed353ac1ced7837632ab78b6e0570
asecurity <mike.olden@gmail.com>
2017-12-13 21:22:29 -0600

    Added notes to source

--- aseccoin.py
+++ aseccoin.py
@@ -2,7 +2,10 @@ from urllib2 import Request, urlopen, URLError
 import json
 #headers = {'User-Agent': 'AsecCoin 0.1'}
 
-request = Request('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-ltc')
+# Break out api URL from $market to loop through list
+request = Request('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-pivx')
+
+# Create a dictionary/list of markets to loop through
 
 try:
     response = urlopen(request)
@@ -21,9 +24,6 @@ try:
         print("High:   %f" %result[0]["High"])
         print("Low:    %f" %result[0]["Low"])
 
-        #print(json.dumps(result, ensure_ascii=True, indent=4, sort_keys=True, separators=(',', ':')))
-        #var = json.dumps(result, ensure_ascii=True, indent=4, sort_keys=True, separators=(',', ':'))
-        #test = var['Ask']
     else: 
         print "Parsing failed..."
 
